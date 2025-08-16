b954210d9277d127fdbc135fb959e02df0dabe9c
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2025-08-16 16:08:36 +0300

    WIP commit

--- src/grid.py
+++ src/grid.py
@@ -1,79 +1,59 @@
 import numpy as np
 
 class Grid:
-    def __init__(self, resolution=32, bounds=[[0, 1], [0, 1], [0, 1]]):
+    def __init__(self, resolution=32):
         """
         Create a 3D grid of integer indices and associated spatial coordinates.
 
         Parameters:
-        - resolution: int or list [Nx, Ny, Nz]
-        - bounds: list of 3 [min, max] intervals
+        - resolution: int
         """
-        if isinstance(resolution, int):
-            self.res = [resolution, resolution, resolution]
-        else:
-            self.res = list(resolution)
-        self.bounds = [list(b) for b in bounds]
+        self.resolution = resolution
+        self.scale: float = 1.0 / self.resolution
+        self.indices_grid, self.indices_flat = self._generate_indices()
+        self.positions_grid, self.positions_flat = self._generate_positions(self.indices_flat)
 
-        self._generate_indices()
-        self._compute_scale_offset()
-        self._compute_positions()
-
-    def _generate_indices(self):
-        nx, ny, nz = self.res
+    def _generate_indices(self) -> tuple:
         i, j, k = np.meshgrid(
-            np.arange(nx), np.arange(ny), np.arange(nz),
+            np.arange(self.resolution), np.arange(self.resolution), np.arange(self.resolution),
             indexing="ij"
         )
-        self.indices_grid = np.stack([i, j, k], axis=-1)      # Shape (Nx, Ny, Nz, 3)
-        self.indices_flat = self.indices_grid.reshape(-1, 3)  # Shape (Nx*Ny*Nz, 3)
+        indices_grid = np.stack([i, j, k], axis=-1)      # Shape (Nx, Ny, Nz, 3)
+        indices_flat = indices_grid.reshape(-1, 3)  # Shape (Nx*Ny*Nz, 3)
+        return (indices_grid, indices_flat)
 
-    def _compute_scale_offset(self):
-        self.scale = np.array([
-            (self.bounds[0][1] - self.bounds[0][0]) / self.res[0],
-            (self.bounds[1][1] - self.bounds[1][0]) / self.res[1],
-            (self.bounds[2][1] - self.bounds[2][0]) / self.res[2],
-        ])
-        self.offset = np.array([
-            self.bounds[0][0],
-            self.bounds[1][0],
-            self.bounds[2][0],
-        ])
-
-    def _compute_positions(self):
-        self.positions_flat = self.indices_flat * self.scale + self.offset
-        self.positions_grid = self.positions_flat.reshape(
-            *self.res, 3
-        )  # Shape (Nx, Ny, Nz, 3)
-
-    def get_flat_indices(self):
-        return self.indices_flat
-
-    def get_grid_indices(self):
-        return self.indices_grid
-
-    def get_flat_positions(self):
-        return self.positions_flat
-
-    def get_grid_positions(self):
-        return self.positions_grid
+    def _generate_positions(self, indices_flat) -> tuple:
+        # From flat indices, generate grid and flat positions.
+        positions_flat = indices_flat * self.scale
+        positions_grid = positions_flat.reshape(self.resolution, self.resolution, self.resolution, 3)
+        return (positions_grid, positions_flat)
 
     def index_to_position(self, idx):
         """
         Convert a single integer index [i, j, k] to spatial coordinates.
+        Wraps indices overflowing from unit cube to create a 3-torus like structure.
         """
-        return np.array(idx) * self.scale + self.offset
+        idx = np.mod(idx, self.resolution)
+        return np.array(idx) * self.scale
 
     def position_to_index(self, pos):
         """
-        Map a point in space to its corresponding grid index (may need rounding).
+        Map a point in space to its corresponding grid index.
+        Takes into account floating point inaccuracies and wraps positions correctly to a 3-torus.
         """
-        idx = (np.array(pos) - self.offset) / self.scale
+        idx = (np.array(pos)) / self.scale
+        idx = np.mod(idx, self.resolution)
         return np.floor(idx).astype(int)
 
+    def evaluate(self, func, return_flat = False) -> np.array:
+        """
+        Get values for function evaluated on each point of the Grid
+        """
+
+        values = np.array([])
+
     def __repr__(self):
-        return f"Grid3D(resolution={self.res}, bounds={self.bounds})"
+        return f"Grid3D(resolution={self.resolution})"
 
 if __name__ == '__main__':
-    # Test grid initialization
-    grid = Grid(resolution=32, bounds=[[-1, 1], [-1, 1], [-1, 1]])
+    grid = Grid(resolution=32)
